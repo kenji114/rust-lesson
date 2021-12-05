394b5d44678659b26fc0db9a9db69c77f6349bef
Kenji SHIMIZU <kenji114@gmail.com>
2021-12-05 20:09:34 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//mod vars;
+mod generics;
+
+fn main() {
+    // println!("Hello, world!");
+    //vars::run();
+    //    vars::sub_a::func_a()
+    //    vars::sub_b::func_a();
+    generics::run();
+}
