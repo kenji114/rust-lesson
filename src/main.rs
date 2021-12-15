25e3ff22309fdcfc250820ee729ca52a92064759
kenji114 <kenji114@gmail.com>
2021-12-15 23:15:58 +0000

    after the lesson of structs.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-//mod vars;
-mod generics;
+// mod vars;
+// mod generics;
+// mod lifetime;
+// mod structs;
+// mod enums;
+mod traits;
 
 fn main() {
     // println!("Hello, world!");
-    //vars::run();
+    // vars::run();
     //    vars::sub_a::func_a()
     //    vars::sub_b::func_a();
-    generics::run();
+    // generics::run();
+    // lifetime::run()
+    // structs::run()
+    // enums::run()
+    traits::run()
 }
---
+++ src/tempCodeRunnerFile.rs
@@ -0,0 +1,4 @@
+fn dummy1<'a>()->&'a str{
+//     let s = String::from("demo");
+//     &s
+// }
